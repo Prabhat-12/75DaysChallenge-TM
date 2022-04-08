c69e9a36a307380f12dcde5132aee5c82b1058ff
Prabhat Motwani <70259205+Prabhat-12@users.noreply.github.com>
2022-04-09 00:08:11 +0530

    Time: 90 ms (56.86%), Space: 48.4 MB (74.60%) - LeetHub

---
+++ 55-jump-game/55-jump-game.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+    //     int dis = 0;
+    // for (int i = 0; i <= dis; i++) {
+    //     dis = max(dis, i + nums[i]);
+    //     if (dis >= nums.size()-1) {
+    //         return true;
+    //     }
+    // }
+    // return false;
+    // }
+        
+        int size = nums.size();
+        if (size == 1) {
+            return true;
+        }
+        int maxJump = 0;
+        for (int i = 0; i < size-1; i++) {
+            if (maxJump < i) {
+                return false;
+            }
+            maxJump = max(maxJump, i+ nums[i]);
+        }
+        if (maxJump >= size-1) {
+            return true;
+        }
+        
+        return false;
+    }
+};
